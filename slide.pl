f394e965fe82c206641a741210843ece505112f6
jrf-g <jrfaragalla@gmail.com>
2026-06-13 10:55:22 -0700

    Add slide content to slide.pl

---
+++ slide.pl
@@ -0,0 +1,8 @@
+my $slide = <<EOF;
+$$%%##############################%%$$
+# Develop                            #
+#        OS                          #
+#          the future of development #
+$$%%##############################%%$$
+EOF
+print "$slide \n"
